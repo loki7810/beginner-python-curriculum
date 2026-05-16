76625ec5db725591458a2e74abb057a4a8761a7a
loki7810 <loki7810@gmail.com>
2026-05-16 02:30:07 +0000

    Homework1

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,35 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+x = 10
+print(x)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+y = 8
+print(y)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+w = "watermelon"
+print("My favorite fruit is " + w + ".")
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+city = "Kenmore"
+country = "USA"
+print(city + "\n" + country)
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet = "Gima"
+age = 2
+petType = "British Shorthair"
+# My cat is Gima, she is 2 years old and her type is a British Shorthair.
+out = "My cat is "+pet+", she is "+str(age)+" years old and her type is a "+petType+"."
+print(out)
+
