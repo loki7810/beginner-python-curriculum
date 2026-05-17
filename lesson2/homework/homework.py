e743190086bada53d8624c63fca635b5e4b945fe
loki7810 <loki7810@gmail.com>
2026-05-17 18:15:14 +0000

    Homework 2 finished

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -3,16 +3,16 @@
 # Print their quotient and remainder on separate lines.
 a=int(input("Enter a number: "))
 b = int(input("Enter another number: "))
-print("Quotient is", a / b)
+print("Quotient is", a // b)
 print("Remainder is", a % b)
 
 
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
-x = str(input("Enter a animal name: "))
-y=str(input("give me a color: "))
-print("I would want a " , y + x)
+x = input("Enter a animal name: ")
+y=input("Give me a color: ")
+print("A", y, x, "would be awesome" )
 
 
 
@@ -25,8 +25,8 @@ print("I would want a " , y + x)
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
-a=int(input("how many push ups can you do ?: "))
-b= print(a * 14)
+a=int(input("How many push ups can you do ?: "))
+print( "You can do", a * 7, "push ups in a week.")
 
 
 
