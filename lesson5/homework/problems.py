f554222a59cfd636b8e651cfc22d12893fa0aa32
loki7810 <loki7810@gmail.com>
2026-06-20 00:08:17 +0000

    1

--- lesson5/homework/problems.py
+++ lesson5/homework/problems.py
@@ -26,20 +26,32 @@ print(subject)
 # Create a list of 5 error codes.
 # Print how many there are.
 # Then find the index of "403" and print it.
-
-
+err = [404, 403, 401, 502, 301]
+length = len(err)
+print("Number of errors:", length)
+index_of_403 = err.index(403)
+print("Index of '403':", index_of_403)
 
 
 # Problem 4 
 # Create a list of 2 programming languages.
 # Print a random one.
 # Then append another language and print the list.
+import random
 
+pl = ["python", "c"]
+random_pl = random.choice(pl)
+print(random_pl)
 
 
 # Problem 5
 # Create a list of 6 passwords.
 # Print the one in the middle using len().
 # Then remove the first password in the list and print it.
+passwwords = ["Iamsmart", "himynameismarcus", "K9#mP7$qL2@vN8*jR5", "qwerty54765**", "Iampoor2348967", "erghieru123$$$"]
+
+
+
+
 
     
