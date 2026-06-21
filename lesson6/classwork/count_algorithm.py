2aa77bf83a906c1b874e07fd7d5730a5c106ed14
loki7810 <loki7810@gmail.com>
2026-06-21 01:01:47 +0000

    finished less 6

---
+++ lesson6/classwork/count_algorithm.py
@@ -0,0 +1,23 @@
+animals = ["cat", "dog", "cat", "tiger", "lion"]
+print(animals)
+
+# You can use built-in Python functions to count some items.
+num_cats = animals.count("cat")
+print("There are", num_cats, "cats.")
+
+print("Our algorithm:")
+
+counter = 0
+for i in range(len(animals)):  # Go through each item in the list.
+    item = animals[i]
+    if item == "cat":  # Check if the item is "cat"
+        counter = counter + 1  # If the item is "cat", add 1 to the counter
+print(counter, "cats")
+
+numbers = [14, 1, 50, 4, 20, 12]
+counter = 0
+for i in range(len(numbers)):
+    item = numbers[i]
+    if item > 10:
+        counter = counter + 1
+print(counter, "numbers above 10")
--- lesson6/classwork/practice.py
+++ lesson6/classwork/practice.py
@@ -2,13 +2,23 @@
 # Count and print how many time "dog" appears in the list.
 pets = ["dog", "cat", "dog", "hamster", "dog", "parrot"]
 print(pets)
+num_dog = pets.count("dog")
+print("There are", num_dog, "dogs.")
+
 
 
 
 # Problem 2
 # Count and print how many numbers are odd in the list (a number is odd if it's not divisible by 2).
 numbers = [8, 3, 12, 7, 4, 11]
+counter = 0
+for i in range(len(numbers)):
+    item = numbers[i]
+    if item:
 print(numbers)
+
+
+
 
 
 
@@ -16,13 +26,29 @@ print(numbers)
 # Search for "monkey" in the list and print its index if it's found.
 zoo = ["lion", "elephant", "monkey", "giraffe", "zebra"]
 print(zoo)
+counter = 0 
+for i in range(len(zoo)):  # Go through each item in the list
+    if zoo[i] == "monkey":  # If the current item is apple
+        found = True  # Mark as found
+        index = i  # Save the index
+        break  # Exit the loop after finding
+if found == True:
+    print("found 1 or more monkey(s)")
+else:
+    print("no monkey found")
+
+
+
 
 
 
 # Problem 4
 # Search for 99 in the list and print if it’s found.
 numbers = [10, 45, 32, 99, 60, 5]
-print(numbers)
+if 99 in numbers:
+    print("found 99")
+else:
+    print("99 not found")
 
 
 
